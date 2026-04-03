97762a276a39b815103343129edf533564014699
neelgupta <neelgupta701@gmail.com>
2026-04-03 13:15:39 +0530

    docker changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,9 @@
 import path from "node:path";
-import type { PrismaConfig } from "prisma";
+import { defineConfig, env } from "prisma/config";
 
-export default {
+export default defineConfig({
   schema: path.join(__dirname, "prisma", "schema.prisma"),
-} satisfies PrismaConfig;
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
